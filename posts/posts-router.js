65e0f1c30fe24710ffd775012a2301c3534972d1
Galo Sandoval <galosan92@gmail.com>
2020-09-01 17:50:59 -0700

    solved both get problems

--- posts/posts-router.js
+++ posts/posts-router.js
@@ -1,12 +1,13 @@
 const express = require("express");
 const router = express.Router();
 const Posts = require("../data/db");
+const { findById } = require("../data/db");
 
 router.get("/", (req, res) => {
   Posts.find()
     .then((posts) => {
       console.log(posts);
-      res.status(200).json( posts );
+      res.status(200).json(posts);
     })
     .catch((error) => {
       console.log(error);
@@ -18,31 +19,33 @@ router.get("/", (req, res) => {
 });
 
 router.get("/:id", (req, res) => {
-  const id = req.params.id
-  Posts.findById()
-    .then((posts) => {
-      console.log(posts);
-      res.status(200).json({ posts });
-    })
-    .catch((error) => {
-      console.log(error);
+  const id = Number(req.params.id);
+  findById(id).then((post) => {
+    if (!post) {
       res
         .status(400)
         .json({ errorMessage: "Please provide text for the comment." })
         .end();
-    });
+    }
+    if (post) {
+      res.status(200).json(post);
+    } else {
+      res.status(500).json({
+        message:
+          '{ errorMessage: "The user information could not be retrieved." }',
+      });
+    }
+  });
 });
 
 console.log(Posts);
 
 router.post("/", (req, res) => {
-  console.log(req.body)
+  console.log(req.body);
   if (!req.body.title || !req.body.contents) {
-    res
-      .status(400)
-      .json({
-        errorMessage: "Please provide title and contents for the post.",
-      });
+    res.status(400).json({
+      errorMessage: "Please provide title and contents for the post.",
+    });
   } else if (req.body.title && req.body.contents) {
     Posts.insert(req.body);
     res.status(201).json(req.body);
@@ -52,6 +55,5 @@ router.post("/", (req, res) => {
     });
   }
 });
-
 
 module.exports = router;
