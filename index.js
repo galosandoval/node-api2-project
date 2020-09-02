0207a66eb82a29547add0e0f240181df2e13c481
Galo Sandoval <galosan92@gmail.com>
2020-09-01 17:07:56 -0700

    fixed bug

--- index.js
+++ index.js
@@ -1,52 +1,16 @@
 const express = require("express");
-const server = express();
-const shortid = require("shortid");
 
 const Posts = require("./data/db");
 const postsRouter = require("./posts/posts-router");
-const commentsRouter = require("./comments/comments-router");
+const server = express();
 
-const blogPosts = [
-  {
-    title: "The post title", // String, required
-    contents: "The post contents", // String, required
-    created_at: "Mon Aug 14 2017 12:50:16 GMT-0700 (PDT)", // Date, defaults to current date
-    updated_at: "Mon Aug 14 2017 12:50:16 GMT-0700 (PDT)", // Date, defaults to current date
-  },
-];
 
-const comment = [
-  {
-    text: "The text of the comment", // String, required
-    post_id: shortid.generate(), // Integer, required, must match the id of a post entry in the database
-    created_at: "Mon Aug 14 2017 12:50:16 GMT-0700 (PDT)", // Date, defaults to current date
-    updated_at: "Mon Aug 14 2017 12:50:16 GMT-0700 (PDT)", // Date, defaults to current date
-  },
-];
-
-// server.get("/", (req, res) => {
-//   const query = req.query;
-//   console.log(query);
-
-//   Posts.find()
-//     .then((posts) => {
-//       res.status(200).json(posts);
-//     })
-//     .catch((error) => {
-//       console.log(error);
-//       res
-//         .status(400)
-//         .json({ errorMessage: "Please provide text for the comment." })
-//         .end();
-//     });
-// });
-
+server.use(express.json());
 server.use("/api/posts", postsRouter);
-// server.use('/api/posts/:id/comments', commentsRouter)
 
-// server.get('/', (req, res) => {
-//   res.status(200).json({message: 'Successfully used GET'})
-// })
+server.get("/", (req, res) => {
+  res.status(200).json({ message: "Successfully used GET" }).end();
+});
 
 const port = 8000;
 server.listen(port, () => console.log("Listening on Port 8000"));
--- posts/posts-router.js
+++ posts/posts-router.js
@@ -3,9 +3,10 @@ const router = express.Router();
 const Posts = require("../data/db");
 
 router.get("/", (req, res) => {
-  Posts.find(query)
+  Posts.find()
     .then((posts) => {
-      res.status(200).json({ data: posts });
+      console.log(posts);
+      res.status(200).json( posts );
     })
     .catch((error) => {
       console.log(error);
@@ -15,5 +16,42 @@ router.get("/", (req, res) => {
         .end();
     });
 });
+
+router.get("/:id", (req, res) => {
+  const id = req.params.id
+  Posts.findById()
+    .then((posts) => {
+      console.log(posts);
+      res.status(200).json({ posts });
+    })
+    .catch((error) => {
+      console.log(error);
+      res
+        .status(400)
+        .json({ errorMessage: "Please provide text for the comment." })
+        .end();
+    });
+});
+
+console.log(Posts);
+
+router.post("/", (req, res) => {
+  console.log(req.body)
+  if (!req.body.title || !req.body.contents) {
+    res
+      .status(400)
+      .json({
+        errorMessage: "Please provide title and contents for the post.",
+      });
+  } else if (req.body.title && req.body.contents) {
+    Posts.insert(req.body);
+    res.status(201).json(req.body);
+  } else {
+    res.status(500).json({
+      error: "There was an error while saving the post to the database",
+    });
+  }
+});
+
 
 module.exports = router;
