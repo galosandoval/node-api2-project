fe012b1cffd3b422caf42a35ded043ff2c5b78e4
Galo Sandoval <galosan92@gmail.com>
2020-09-01 18:40:04 -0700

    finished the delete

--- posts/posts-router.js
+++ posts/posts-router.js
@@ -1,8 +1,9 @@
 const express = require("express");
 const router = express.Router();
 const Posts = require("../data/db");
-const { findById } = require("../data/db");
+const { findById, findPostComments, remove } = require("../data/db");
 
+// GET
 router.get("/", (req, res) => {
   Posts.find()
     .then((posts) => {
@@ -20,14 +21,14 @@ router.get("/", (req, res) => {
 
 router.get("/:id", (req, res) => {
   const id = Number(req.params.id);
+  console.log(id);
   findById(id).then((post) => {
     if (!post) {
       res
         .status(400)
         .json({ errorMessage: "Please provide text for the comment." })
         .end();
-    }
-    if (post) {
+    } else if (post) {
       res.status(200).json(post);
     } else {
       res.status(500).json({
@@ -38,7 +39,25 @@ router.get("/:id", (req, res) => {
   });
 });
 
+router.get("/:id/comments", (req, res) => {
+  const id = req.params.id;
+  console.log(id);
+  Posts.findCommentById(id).then((post) => {
+    if (post.length === 0) {
+      res
+        .status(404)
+        .json({ message: "The post with the specified ID does not exist." });
+    } else {
+      // Posts.findCommentById(id).then((comments) =>
+      res.status(201).json({ comments });
+      // );
+    }
+  });
+});
+
 console.log(Posts);
+
+// POST
 
 router.post("/", (req, res) => {
   console.log(req.body);
@@ -54,6 +73,30 @@ router.post("/", (req, res) => {
       error: "There was an error while saving the post to the database",
     });
   }
+});
+
+router.post("/:id/comments", (req, res) => {
+  const id = Number(req.params.id);
+  findPostComments(id);
+});
+
+// DELETE
+
+router.delete("/:id", (req, res) => {
+  const id = Number(req.params.id);
+  remove(id)
+    .then((post) => {
+      if (!post) {
+        res
+          .status(404)
+          .json({ message: "The post with the specified ID does not exist." });
+      } else {
+        res.json({ message: "Post successfuly deleted" });
+      }
+    })
+    .catch((error) => {
+      res.status(500).json({ error: "The post could not be removed" });
+    });
 });
 
 module.exports = router;
